bda9e944ec6be7cf04532c9627cd1ec2ab4b742a
Priscila da Rocha Alves <priscila.mizuiky@gmail.com>
2024-02-23 10:29:11 -0300

    Change item manager to be get the update item responsible from itembuilder

--- Inventory/Assets/_Development/Scripts/Managers/ItemManager.cs
+++ Inventory/Assets/_Development/Scripts/Managers/ItemManager.cs
@@ -1,36 +1,84 @@
+using NaughtyAttributes;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using static UnityEditor.Progress;
 
-public class ItemManager : MonoBehaviour
+public class ItemManager
 {
-    public List<ItemBase> items;
-    public Action<IInventoryItem> OnUpdateItem;
+    public List<ItemBase> _items;
+    public List<InventoryItem> _inventoryItems;
+    public Action<GameObject, int> OnUpdateItem;
 
     public void Init()
     {
-        items = new List<ItemBase>();
+        _items = new List<ItemBase>();
+        _inventoryItems = new List<InventoryItem>();
     }
 
-    public void AddItem(ItemBase item)
+    public void UpdateQtd(ItemBase item)
     {
-        var currentItem = items.FirstOrDefault(x => x.Data.id == item.Data.id);
+        var currentItem = _items.FirstOrDefault(x => x.Data.id == item.Data.id);
         
         if(currentItem == null)
         {
             currentItem = item;
-            items.Add(currentItem);
-        }      
-        
-        currentItem.Data.inventoryItemData.qtd += 1;
+            AddItem(currentItem);
+        }
 
-        UpdateInventory(currentItem.InventoryItem);
+        var inventoryItem = _inventoryItems.FirstOrDefault(x => x.Data.id == item.Data.id);
+        inventoryItem.Qtd += 1;
+
+        UpdateInventory(inventoryItem.gameObject, inventoryItem.Data.id);
     }
 
-    public void UpdateInventory(IInventoryItem item)
+    public void UpdateInventory(GameObject item, int id)
     {
-        OnUpdateItem?.Invoke(item);
+        OnUpdateItem?.Invoke(item, id);
+    }
+
+    public void FillItemList(List<ItemBase> itemBase, List<InventoryItem> inventoryItems)
+    {
+        _items = itemBase;
+        _inventoryItems = inventoryItems;
+    }
+
+    public void AddItem(ItemBase item)
+    {
+        _items.Add(item);
+    }
+
+    public void AddNewItem(ItemBase item, InventoryItem newInventoryItem)
+    {
+        _items.Add(item);
+        _inventoryItems.Add(newInventoryItem);
+    }
+
+    public ItemBase GetItemAtIndex(int index)
+    {
+        return _items[index];
+    }
+
+    public InventoryItemData GetInventoryDataById(int id)
+    {
+        return _inventoryItems.FirstOrDefault(x=>x.Data.id == id).Data;
+    }
+
+    public void UpdateInventoryItemList(InventoryItemData itemData)
+    {
+        var itemToUpdate = _items.FirstOrDefault(x => x.Data.id == itemData.id).Data.inventoryData;
+        itemToUpdate = itemData;
+
+        var InventoryItemToUpdate = _inventoryItems.FirstOrDefault(x => x.Data.id == itemData.id);
+        InventoryItemToUpdate.Data = itemData;
+
+        SaveItems();
+    }
+
+    public void SaveItems()
+    {
+        var list = _items.Select(x => x.Data).ToList();
+        GameManager.Instance.ItemBuilder.SaveData(list);
     }
 }
