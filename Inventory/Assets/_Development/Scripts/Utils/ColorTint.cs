7e5622ec48fcfc8209e654974fb33b602c596249
Priscila da Rocha Alves <priscila.mizuiky@gmail.com>
2024-03-14 16:23:38 -0300

    add separated script to handle the function to change item color

---
+++ Inventory/Assets/_Development/Scripts/Utils/ColorTint.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorTint : MonoBehaviour
+{
+    private Dictionary<int, Color> _itemColors;
+    private Image _image;
+
+    private Color _originalColor;
+    private Color _selectedColor;
+    private Color _cantPlaceColor;
+
+    public void Init(Image image)
+    {
+        _image = image;
+        _originalColor = image.color;
+        _selectedColor = ColorToHex("#3EDB83");
+        _cantPlaceColor = ColorToHex("#DB3D3D");
+    }
+
+    private Color ColorToHex(string colorHexadecimal)
+    {
+        Color color;
+
+        if (ColorUtility.TryParseHtmlString(colorHexadecimal, out color))
+        {
+            return color;
+        }
+
+        return _originalColor;
+    }
+
+    public void SetColor(ColorType type)
+    {
+        switch(type)
+        {
+            case ColorType.SelectedItem: _image.color = _selectedColor;
+                break;
+            case ColorType.ReleasedItem: _image.color = _originalColor;
+                    break;
+            case ColorType.CantPlaceItem: _image.color = _cantPlaceColor;
+                break;
+        }       
+    }
+}
