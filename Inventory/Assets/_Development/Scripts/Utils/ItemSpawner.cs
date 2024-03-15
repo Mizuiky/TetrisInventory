6a73ffddd081b2d24b3fa60dd0325999e75c1d96
Priscila da Rocha Alves <priscila.mizuiky@gmail.com>
2024-03-15 10:25:11 -0300

    Spawner refactor

---
+++ Inventory/Assets/_Development/Scripts/Utils/ItemSpawner.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class ItemSpawner : MonoBehaviour
+{
+    public int ItemIdToSpawn;
+
+    public GameObject Spawn(GameObject objToSpawn, Transform parent)
+    {
+        return Instantiate(objToSpawn, parent);
+    }
+
+    public void AddItemToInventory()
+    {
+        var item = GameManager.Instance.ItemManager.GetItemById(ItemIdToSpawn);
+        GameManager.Instance.ItemManager.SendItemDataToInventory(item);
+    }
+
+    public InventoryItem SpawnInventoryItem(InventoryItem item, Transform _inventoryItemsParent)
+    {
+        var newItem = Spawn(item.gameObject, _inventoryItemsParent);
+
+        if (newItem != null)
+        {
+            var itemToAdd = newItem.GetComponent<InventoryItem>();
+            itemToAdd.Data = item.Data;
+
+            itemToAdd.SetComponents();
+            return itemToAdd;
+        }
+
+        return null;
+    }
+}
